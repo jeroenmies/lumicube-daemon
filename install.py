a8d25ff4b1ec0a824dec0af4ac440ec31fc30148
Jeroen Mies <jeroen.mies@gmail.com>
2024-06-30 19:46:23 +0200

    Remove download autumn.jpg

--- install.py
+++ install.py
@@ -199,11 +199,11 @@ def install_addon_packages():
     run('unzip -o ' + vosk_model_path + ' -d ' + voice_directory)
     run('tar -zxvf ' + precise_engine_path + ' -C ' + voice_directory)
 
-    autumn_image = 'autumn.jpg'
-    autumn_image_path = os.path.join(desktop_directory, autumn_image)
-    run('curl -fL '
-        + shlex.quote(AUTUMN_IMAGE_URL_PREFIX + autumn_image)
-        + ' > ' + autumn_image_path)
+#    autumn_image = 'autumn.jpg'
+#    autumn_image_path = os.path.join(desktop_directory, autumn_image)
+#    run('curl -fL '
+#        + shlex.quote(AUTUMN_IMAGE_URL_PREFIX + autumn_image)
+#        + ' > ' + autumn_image_path)
 
 
 install_system_dependencies()
